95d5fbdb142aca3a708d8866a422af9861e5ff1e
Marvischief <igbokwemarvis2239@gmail.com>
2023-06-15 06:44:23 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,9 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"
-			Programming is like building a multilingual puzzle");
+Programming is like a multilingual puzzle");
 	return (0);
-} 
+}
