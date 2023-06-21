d055b7bda726fd552aa0d2db8afd7357d72f175d
Marvischief <igbokwemarvis2239@gmail.com>
2023-06-21 04:31:32 -0700

    hehe operators error

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,7 +13,7 @@ int main(void)
 	unsigned long int i;
 	unsigned long int bef = 1;
 	unsigned long int aft = 2;
-	unsigned long int l = 1000000000;
+	unsigned long int l = 10000000000;
 	unsigned long int bef1;
 	unsigned long int bef2;
 	unsigned long int aft1;
@@ -39,9 +39,9 @@ int main(void)
 		printf(", %lu", aft1 + (aft2 / l));
 		printf("%lu", aft2 % l);
 		aft1 = aft1 + bef1;
-		bef1 = aft1 = bef1;
+		bef1 = aft1 - bef1;
 		aft2 = aft2 + bef2;
-		bef2 = aft2 = bef2;
+		bef2 = aft2 - bef2;
 	}
 	printf("\n");
 	return (0);
